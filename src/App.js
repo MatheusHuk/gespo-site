c9de2e56eab175d4f441a2b3c008244d8e07a925
marcorover <marcomorover@gmail.com>
2020-03-31 19:23:00 -0300

    cards do react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import MenuBar from './components/menuBar';
 import CarouselBack from './components/carousel';
+import CartaoBack from './components/cartao';
+import { Container, Row, Col } from 'react-bootstrap';
+import * as img from './assets/images';
 
 function App() {
   return (
 	<>
 		<MenuBar />
 		<CarouselBack />
+		<Container className= "container-card">
+			<Row>
+				<Col><CartaoBack className = "cor-card" titulo = {"QUEM"}
+								 subTitulo = {"Somos?"}	
+								 texto = {"Descrição Básica"}
+								 textoBotao = {"Nossa História"}
+								 textoDetalhado = {"Descrição detalhada"}
+								 icone = {img.work}/>
+								 
+								 
+				</Col>
+				<Col ><CartaoBack titulo = {" O QUE"}
+								 subTitulo = {"Fazemos?"}	
+								 texto = {"Descrição Básica"}
+								 textoBotao = {"Nossos Serviços"}
+								 textoDetalhado = {"Descrição detalhada"}
+								 icone = {img.support}/>
+				</Col>
+				<Col ><CartaoBack titulo = {"COMO"}
+								 subTitulo = {"Se Associar?"}	
+								 texto = {"Descrição Básica"}
+								 textoBotao = {"Associe-se"}
+								 textoDetalhado = {"Descrição detalhada"}
+								 icone = {img.member}/>
+				</Col>
+			</Row>
+		</Container>	
 	</>
   );
 }
--- src/assets/images.js
+++ src/assets/images.js
@@ -2,3 +2,6 @@ export { default as wood } from './wood.jpg';
 export { default as s1 } from './slide1.jpg';
 export { default as s2 } from './slide2.jpg';
 export { default as s3 } from './slide3.jpg';
+export {default as work} from './work.png';
+export {default as support} from './support.png';
+export {default as member} from './membership.png';
